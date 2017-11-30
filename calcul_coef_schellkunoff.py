489ea4df4629e67913f81f6a4f08d5d7dc68ba80
Antoine Caillon <32771520+caillonantoine@users.noreply.github.com>
2017-11-30 13:04:56 +0100

    Passage en mode automatique

--- calcul_coef_schellkunoff.py
+++ calcul_coef_schellkunoff.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sympy
+z = sympy.Symbol("z")
+poly = sympy.poly
+#%%
 import pickle as pk
 import numpy as np
-def main(z,poly):
-	k = 2/float(input("lambda? "))
-	n = int(input("n? "))
-	d = float(input("d? "))
+import functools
+
+def multiplie(liste):
+    y = 1
+    for elm in liste:
+        y *= elm
+    return y
+
+def main(k,n,d):
 
 	def get_root(i):
 		return np.exp(-np.pi*1j*(-2*k*d*i/(float(n-1))))
 
-	expression = reduce(lambda x,y:x*y,[z-get_root(i) for i in np.arange(1,6)])
+	expression = multiplie([z-get_root(i) for i in np.arange(1,n+1)])
 
 	coef = poly(expression,z).coeffs()
-	with open("coef.txt","w") as output:
-		 pk.dump([complex(elm) for elm in coef],output,0)
+	with open("coef.txt","wb") as output:
+            pk.dump([complex(elm) for elm in coef],output,0)
+            print("ok")
+
+main(340/5000.,11,.1)
