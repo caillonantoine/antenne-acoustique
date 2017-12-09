bd282bc9bb1b4a8c262819c3a77fff9c3dad0cb6
Antoine <caillonantoine@gmail.com>
2017-12-09 11:53:06 +0000

    polar -> VALIDATION() et passage à la directivité moyenné

--- polar.py
+++ polar.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 30 11:42:24 2017
-
-@author: gdias
-"""
 
 #coding:utf-8
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 import pickle
+
 
 
 class Wave(object):
@@ -56,11 +52,51 @@ def get_intensity_around_circle(r,pressure,pas,period):
     #On somme les pressions pour tous les intervalles de temps
     intensity = sum([np.power(pressure(circlex,circley,t),2)\
     for t in np.linspace(0,period,pas)])/float(pas)
-    return space,np.sqrt(intensity)
+    intensity = np.sqrt(intensity)/np.mean(np.sqrt(intensity))
+    return space,intensity
     
 #%%
+
+def validation():
+    f=4000
+    lamb=340/f
+    onde = Wave(1,2*pi*f,2*pi/lamb)
+
+    poles = []
+    poles.append(Pole(.001,0,True))
+    poles.append(Pole(-.001,0,False))
     
-if __name__ == "__main__":
+    #On définit une fonction donnant la pression en x,y,t
+    get_source_pressure = lambda x,y,t: sum([get_pressure(onde,x,y,t,p.x,p.y,\
+    opposite_phase=p.opp_phase,phase=p.phase,ponderation=p.ponderation) for p in poles])
+    
+    zoom_factor = 1
+    #On représente la source d'émissions
+    x = np.linspace(-zoom_factor,zoom_factor,1000) #SCALE X
+    y = np.linspace(-zoom_factor,zoom_factor,1000) #SCALE Y
+    xx,yy = np.meshgrid(x,y) #SCALE XY
+    
+    source = get_source_pressure(xx,yy,1.5) #On évalue la pression instantanée pour t=1
+    #PLOT DE LA SOURCE
+    plt.imshow(source,cmap='Blues',vmin=-10,vmax=10,extent=[x[0],x[-1],y[0],y[-1]])
+    plt.title("Representation 2D de la source pour $f={}$".format(f))
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.show()
+    
+    #PLOT DE LA DIRECTIVITE DE LA SOURCE    
+    #On récupère les valeurs d'intensité
+    intensity = get_intensity_around_circle(2,get_source_pressure,1000,1/f)
+    plt.polar(*intensity,color='orange')
+
+    space = np.linspace(0,2*pi,1000)
+    directivite = abs(np.cos(space))/np.mean(abs(np.cos(space)))
+    plt.polar(space,directivite,color='blue')
+    plt.legend(["Simulation","Theorie"])
+    plt.savefig("validation.svg")
+    plt.show()
+  
+def ponderation():
     f=5000. #Définition de la fréquence
     longueur_onde = 340/f
     with open('coef.txt','rb') as text:
